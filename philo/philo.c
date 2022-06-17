635b2d5fac8c2170f9e3a3311b37c293fee62fcf
hassan khalil <hkhalil@student.1337.ma>
2022-06-17 01:27:36 +0100

    initial commit

--- philo/philo.c
+++ philo/philo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: hkhalil <hkhalil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 04:01:55 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/06/15 04:09:38 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/06/17 01:22:38 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 
 int	main(int argc, char *argv[])
 {
+	t_arguments args;
 
+	args.number_of_philosophers = ft_atoi(argv[1]);
+	args.time_to_die = ft_atoi(argv[2]);
+	args.time_to_eat = ft_atoi(argv[3]);
+	args.time_to_sleep = ft_atoi(argv[4]);
+	args.number_of_times_each_philosopher_must_eat = ft_atoi(argv[5]);
+	
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -3,14 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: hkhalil <hkhalil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 04:09:46 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/06/15 04:46:45 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/06/17 01:11:43 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+# include <pthread.h>
+# include <stdlib.h>
+# include <stdio.h>
 
+typedef struct s_arguments
+{
+    int number_of_philosophers;
+    int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+    int number_of_times_each_philosopher_must_eat;
+}   t_arguments;
 #endif
