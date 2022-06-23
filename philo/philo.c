3beebe7e7e96aaccf8cfef0991e1adf7ebc2dcee
hassan khalil <hkhalil@student.1337.ma>
2022-06-23 18:11:16 +0100

    writing algo (for the routine of threads)

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: hkhalil <hkhalil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 04:01:55 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/06/23 17:39:17 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/06/23 18:10:49 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,25 @@
 void *routine(void *args)
 {
 	t_arguments *s;
+	int			n;
 
 	s = (t_arguments *)args;
+	if ((*s).philosopher_index + 1 == (*s).number_of_philosophers)
+		n = 0;
+	else
+		n = (*s).philosopher_index;
 	while (1)
 	{
-		pthread_mutex_lock(&((*s).fork[(*s).philosopher_index]));
-		printf("%d has taken a fork\n", (*s).philosopher_index + 1);
-		pthread_mutex_lock(&((*s).fork[(*s).philosopher_index + 1]));
-		printf("%d has taken a fork\n", (*s).philosopher_index + 1);
-		printf("%d is eating\n", (*s).philosopher_index + 1);
+		pthread_mutex_lock(&(((*s).fork)[(*s).philosopher_index]));
+		printf("%d has taken a fork\n", n + 1);
+		pthread_mutex_lock(&(((*s).fork)[n + 1]));
+		printf("%d has taken a fork\n", n + 1);
+		//pthread_mutex_lock(&((*s).fork[(*s).philosopher_index + 1]));
+		//printf("%d has taken a fork\n", (*s).philosopher_index + 1);
+		printf("%d is eating\n", n + 1);
 		usleep(((*s).time_to_eat) * 1000);
-		pthread_mutex_unlock(&((*s).fork[(*s).philosopher_index]));
-		pthread_mutex_unlock(&((*s).fork[(*s).philosopher_index + 1]));
+		pthread_mutex_unlock(&(((*s).fork)[(*s).philosopher_index]));
+		pthread_mutex_unlock(&(((*s).fork)[n + 1]));
 		//if (philo is still alive)
 		//	->eat
 		//else
@@ -35,9 +42,9 @@ void *routine(void *args)
 		//	//free
 		//	exit(0);
 	//	}
-		printf("%d is sleeping\n", (*s).philosopher_index + 1);
+		printf("%d is sleeping\n", n + 1);
 		usleep(((*s).time_to_sleep) * 1000);
-		printf("%d is thinking\n", (*s).philosopher_index + 1);
+		printf("%d is thinking\n", n + 1);
 	}
 }
 int	main(int argc, char *argv[])
@@ -55,14 +62,13 @@ int	main(int argc, char *argv[])
 	if (argc == 6)
 		(*args).number_of_times_each_philosopher_must_eat = ft_atoi(argv[5]);
 	(*args).th = malloc(sizeof(pthread_t) * (*args).number_of_philosophers);
-	(*args).fork = malloc(sizeof(pthread_mutex_t) * (*args).number_of_philosophers);
+	(*args).fork = malloc(sizeof(pthread_mutex_t *) * (*args).number_of_philosophers);
 	while (i < (*args).number_of_philosophers)
 	{
-		printf("here  %d\n", i);
 		pthread_mutex_init(&(((*args).fork)[i]), NULL);
 		i++;
 	}
-	(*args).philosopher_index= 0;
+	(*args).philosopher_index = 0;
 	while ((*args).philosopher_index < (*args).number_of_philosophers)
 	{
 		if (pthread_create(&((*args).th)[(*args).philosopher_index], NULL, &routine, &args))
