36066d355f2b9c5306396ab74de0d321bdd19d31
hassan khalil <hkhalil@student.1337.ma>
2022-08-01 18:04:12 +0100

    norm

--- philo/check_for_errors.c
+++ philo/check_for_errors.c
@@ -6,50 +6,46 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 16:20:11 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/08/01 17:45:37 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/08/01 18:01:18 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int is_number(char *s)
+int	is_number(char *s)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (s[i])
-    {
-        if (s[i] > '9' || s[i] < '0')
-            return (-1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] > '9' || s[i] < '0')
+			return (-1);
+		i++;
+	}
+	return (0);
 }
 
-int check_for_errors(int argc, char *argv[])
+int	check_for_errors(int argc, char *argv[])
 {
-    if (argc != 5 && argc != 6)
-    {
-        printf("Error\n");
-        return (-1);
-    }
-    if (is_number(argv[1]) || is_number(argv[2]) || is_number(argv[3]) || is_number(argv[4]))
-    {
-        printf("Error\n");
-        return(-1);
-    }
-    if (ft_atoi(argv[1]) < 1 || ft_atoi(argv[2]) < 0 || ft_atoi(argv[3]) < 0 || ft_atoi(argv[4]) < 0)
-    {
-        printf("Error\n");
-        return(-1);
-    }
-    if (argc == 6)
-    {
-        if (is_number(argv[5]) || ft_atoi(argv[5]) < 0)
-        {
-            printf("Error\n");
-            return (-1);
-        }
-    }
-    return (0);
+	if (argc != 5 && argc != 6)
+	{
+		printf("Error\n");
+		return (-1);
+	}
+	if (is_number(argv[1]) || is_number(argv[2]) || is_number(argv[3]) || is_number(argv[4])
+    || ft_atoi(argv[1]) < 1 || ft_atoi(argv[2]) < 0 || ft_atoi(argv[3]) < 0 || ft_atoi(argv[4]) < 0)
+	{
+		printf("Error\n");
+		return (-1);
+	}
+	if (argc == 6)
+	{
+		if (is_number(argv[5]) || ft_atoi(argv[5]) < 0)
+		{
+			printf("Error\n");
+			return (-1);
+		}
+	}
+	return (0);
 }
